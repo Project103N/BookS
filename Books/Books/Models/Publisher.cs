856dbad174a0c4864e1f2b600e60f67a5b11caf6
GamzeKural <gamzekural98@gmail.com>
2022-02-28 19:42:56 +0300

    Publisher Domain Class

---
+++ Books/Books/Models/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class Publisher
+    {
+        public int PublisherID { get; set; }
+        public string PublisherName { get; set; }
+    }
+}
