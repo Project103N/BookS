76dffb1946b4f6b55659dada0034291dd431e75e
salihkoc <salihkoc.e@gmail.com>
2022-03-01 16:48:22 +0300

    Book domain class added.

---
+++ Books/Books/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public Author Authors { get; set; }
+        public int PageCount { get; set; }
+        public int UnitPrice { get; set; }
+        public string Details { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int UnitsInStock { get; set; }
+        public bool IsActive { get; set; }
+        public int TotalSell { get; set; }
+    }
+}
