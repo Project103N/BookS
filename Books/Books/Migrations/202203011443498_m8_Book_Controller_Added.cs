a113d74d338dd56ab1f3ee4a2cffbdaa6b57fa69
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-03-01 17:46:53 +0300

    BookControllerAdded
    BookControllerAdded

---
+++ Books/Books/Migrations/202203011443498_m8_Book_Controller_Added.cs
@@ -0,0 +1,16 @@
+namespace Books.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class m8_Book_Controller_Added : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
