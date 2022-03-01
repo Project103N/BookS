c304194a5b3de5e2fe416747823a8b3a563b0eeb
Fatih Topcu <fthtpu@gmail.com>
2022-03-01 19:26:46 +0300

    Order Class and Controller added

---
+++ Books/Books/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int AddressID { get; set; }
+        public Address Address { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
