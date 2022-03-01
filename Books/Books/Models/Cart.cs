116827650818aa5d9300f3ae32b39e8160d35ba9
talhazengin77 <talha77zengin@gmail.com>
2022-03-01 17:57:19 +0300

    cart

---
+++ Books/Books/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class Cart
+    {
+        public int TotalPrice { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
