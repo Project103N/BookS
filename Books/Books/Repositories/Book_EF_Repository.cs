0d207a9e2fddea01341508de1b7ff6bfa67470cd
Mehmet DEMİR <mail.mehmetdemir@gmail.com>
2022-03-03 20:43:18 +0300

    Repository pattern : Interface added. 2 repository class added.

---
+++ Books/Books/Repositories/Book_EF_Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Books.Models;
+
+namespace Books.Repositories
+{
+    public class Book_EF_Repository : IRepository<Book, int>
+    {
+        private ApplicationDbContext _db { get; set; }
+        public ApplicationDbContext db
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new ApplicationDbContext();
+                return _db;
+            }
+        }
+
+        public void Add(Book entity)
+        {
+            db.Books.Add(entity);
+            db.SaveChanges();
+        }
+
+        public IEnumerable<Book> Get()
+        {
+            return db.Books.Include(b => b.Author).Include(b => b.Publisher).ToList() ;
+
+        }
+
+        public Book Get(int id)
+        {
+            return db.Books.Find(id);
+        }
+
+        public void Remove(Book entity)
+        {
+            db.Books.Remove(entity);
+            db.SaveChanges();
+        }
+
+        public void Update(Book entity)
+        {
+            var updatedBook = db.Books.Attach(entity);
+            db.Entry(updatedBook).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Books/Books/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Repositories
+{
+    public interface IRepository<TEnt,in TPk> where TEnt:class
+    {
+        IEnumerable<TEnt> Get();
+        TEnt Get(TPk id);
+        void Add(TEnt entity);
+        void Remove(TEnt entity);
+        void Update(TEnt entity);
+    }
+}
