6cb004450d8b2b4634a6eb58bfd1d2d7fd34d66f
talhazengin77 <talha77zengin@gmail.com>
2022-03-07 17:13:35 +0300

    address repositoy added.

---
+++ Books/Books/ViewModels/AuthorPageContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Books.Models;
+
+namespace Books.ViewModels
+{
+    public class AuthorPageContent
+    {
+        public ICollection<Book> AuthorsBook { get; set; }
+        public Author author { get; set; }
+    }
+}
